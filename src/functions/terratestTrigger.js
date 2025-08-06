86aa5cf7d8d805783a6d74c18b22192afdf45e01
Ruturaj Ghodke <59768319+shotcaller@users.noreply.github.com>
2025-08-06 14:32:41 -0300

    Update terratestTrigger.js

--- src/functions/terratestTrigger.js
+++ src/functions/terratestTrigger.js
@@ -1,1 +1,19 @@
 
+    // src/functions/myHttpTrigger.js
+    const { app } = require('@azure/functions');
+
+    app.http('terratestTrigger', {
+        methods: ['GET', 'POST'],
+        authLevel: 'anonymous', // Or 'function', 'admin'
+        handler: async (request, context) => {
+            context.log(`Http function processed request for url "${request.url}"`);
+
+            const name = request.query.get('name') || (request.body && request.body.name);
+
+            if (name) {
+                return { body: `Hello, ${name}!` };
+            } else {
+                return { body: "Please pass a name on the query string or in the request body." };
+            }
+        }
+    });
