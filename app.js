3441597c793308162bcde460e95e91b4b682f84a
Ruturaj Ghodke <59768319+shotcaller@users.noreply.github.com>
2025-08-06 14:29:10 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,5 @@
 const { app } = require('@azure/functions');
 
-app.http('myHttpTrigger', {
-    methods: ['GET', 'POST'],
-    authLevel: 'anonymous',
-    handler: async (request, context) => {
-        context.log(`Http function processed request for url "${request.url}"`);
-
-        const name = request.query.get('name') || (request.body && request.body.name);
-
-        if (name) {
-            return { body: `Hello, ${name}!` };
-        } else {
-            return { status: 400, body: 'Please pass a name on the query string or in the request body' };
-        }
-    }
-});
+app.setup({
+    enableHttpStream: true
+})
